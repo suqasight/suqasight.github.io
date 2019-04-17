e492418a685a1f66373d289ff9952f5d7d8bc7c8
Jerrica Bobadilla <>
2019-04-17 00:55:43 -0700

    Updates

---
+++ precache-manifest.4cbaeccaa128db8e063e3feeca2b1018.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2c7b296f5e97a21d5fd0d7a3bca506b",
+    "url": "/static/media/bst.d2c7b296.jpg"
+  },
+  {
+    "revision": "393239c9b088a7b8b43c",
+    "url": "/static/css/main.244820ab.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9db518e28b888f51e5fc",
+    "url": "/static/js/2.e88f53bf.chunk.js"
+  },
+  {
+    "revision": "f5fee65981f430c4b02643daf2037871",
+    "url": "/static/media/one.f5fee659.png"
+  },
+  {
+    "revision": "75d4e7cdd61644d2bdbbb58a79864d79",
+    "url": "/static/media/two.75d4e7cd.png"
+  },
+  {
+    "revision": "96b65ea305e4f2d1c8bd7f06e0e068f2",
+    "url": "/static/media/three.96b65ea3.png"
+  },
+  {
+    "revision": "0b1b6746dc931dbe18774ec3bb5d694f",
+    "url": "/static/media/four.0b1b6746.png"
+  },
+  {
+    "revision": "e9815fe4f3d5291abcbaa6cabd47eec1",
+    "url": "/static/media/five.e9815fe4.png"
+  },
+  {
+    "revision": "02b9327f78d2645f4e1374618bc5e5f3",
+    "url": "/static/media/six.02b9327f.png"
+  },
+  {
+    "revision": "2b745ae7ed7b38bf984392634211106d",
+    "url": "/static/media/seven.2b745ae7.png"
+  },
+  {
+    "revision": "639e6e217535886a85fda15c8d565199",
+    "url": "/static/media/banner.639e6e21.png"
+  },
+  {
+    "revision": "f0d718edb471cf0262cd912804ead793",
+    "url": "/static/media/airport.f0d718ed.jpg"
+  },
+  {
+    "revision": "d568d331684e1b1d1a7d555aa697b9cb",
+    "url": "/static/media/answers.d568d331.jpg"
+  },
+  {
+    "revision": "ca8cc913acc1c6a3c7f28155d56f96a8",
+    "url": "/static/media/babyoongi.ca8cc913.jpg"
+  },
+  {
+    "revision": "393239c9b088a7b8b43c",
+    "url": "/static/js/main.797ae52f.chunk.js"
+  },
+  {
+    "revision": "2576656dd00f2ad922f42100e1bd00d2",
+    "url": "/static/media/bt21game.2576656d.jpg"
+  },
+  {
+    "revision": "7d59197b66f46c04882d8a852852cf6a",
+    "url": "/static/media/btseasons.7d59197b.jpg"
+  },
+  {
+    "revision": "f1b79b7d1af54b0947b0211ab86defd1",
+    "url": "/static/media/btssaid.f1b79b7d.jpg"
+  },
+  {
+    "revision": "cced027bd87e94549a90b8eedeb7b96c",
+    "url": "/static/media/chimmycloset.cced027b.jpg"
+  },
+  {
+    "revision": "673224f180112e06a0e1f07cab666518",
+    "url": "/static/media/cookyartist.673224f1.jpg"
+  },
+  {
+    "revision": "da81357b478b21dec7d930cb054024fe",
+    "url": "/static/media/fakelove.da81357b.jpg"
+  },
+  {
+    "revision": "0bc3a114ce878af1d4efc2b386bd201d",
+    "url": "/static/media/guccitae.0bc3a114.jpg"
+  },
+  {
+    "revision": "94afada610b64d681af928309423be7f",
+    "url": "/static/media/hobieyes.94afada6.jpg"
+  },
+  {
+    "revision": "a9ad48a40e4399afdaf9b30e12d98960",
+    "url": "/static/media/kookie.a9ad48a4.jpg"
+  },
+  {
+    "revision": "14ca623e73836a12c2e1d9ca8cada870",
+    "url": "/static/media/lydonuts.14ca623e.jpg"
+  },
+  {
+    "revision": "33b6f4d57f94334f8a76b0c3f921d014",
+    "url": "/static/media/missjoon.33b6f4d5.jpg"
+  },
+  {
+    "revision": "75ad55fe463683f246211d65e1f08b2f",
+    "url": "/static/media/seesaw.75ad55fe.jpg"
+  },
+  {
+    "revision": "71b2afb204e6dd9856b62ad6e60e0966",
+    "url": "/static/media/serendipity.71b2afb2.jpg"
+  },
+  {
+    "revision": "1381fc38db32a99dcf9e06aee6504dc3",
+    "url": "/static/media/tearopc.1381fc38.jpg"
+  },
+  {
+    "revision": "f75aee9f1c9df37e70623a1f130f87bc",
+    "url": "/index.html"
+  }
+];
